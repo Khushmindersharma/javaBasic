a63965076cde2b89d00737fb1297e48d342d4615
Khushminder Sharma <sharma_khushminder@yahoo.com>
2026-03-19 18:05:59 -0400

    New Folder

---
+++ src/main/java/com/example/demo/SendevopsApplication.java
@@ -0,0 +1,12 @@
+package com.example.khushdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class khushdevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(khushdevopsApplication.class, args);
+    }
+}

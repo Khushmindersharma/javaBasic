24098d861744d5cf87eba0a9f1243189ff816921
Khushminder Sharma <sharma_khushminder@yahoo.com>
2026-03-20 18:14:24 -0400

    Folder Committed

--- src/main/java/com/example/demo/khushdevopsApplication.java
+++ src/main/java/com/example/demo/khushdevopsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class khushdevopsApplication {
+public class SendevopsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(khushdevopsApplication.class, args);
+        SpringApplication.run(SendevopsApplication.class, args);
     }
 }
